07ddd5fc380e14a4285b1bb2b8b08a41baf11c7b
DanielFachieri01 <138767767+DanielFachieri01@users.noreply.github.com>
2025-08-04 13:19:43 -0300

    Branches to TTrees
    mudei o jeito de guardar os dados. Ao invés de uma TTree com várias branchs para condições diferentes eu criei cada TTree uma condição diferente com uma única branch com o mesmo nome.

--- input_gain.cpp
+++ input_gain.cpp
@@ -8,9 +8,10 @@
 #include <tuple>
 #include <string>
 #include <sstream>
+#include <TF1.h>
+#include <TLatex.h>
 
 void input_gain() {
-    // Condições originais
     std::vector<std::tuple<double, double, double, std::string>> conditions = {
         {1.0, 1, 70, "Ar"},
         {3.0, 1, 70, "Ar"},
@@ -27,50 +28,46 @@ void input_gain() {
         names.push_back(oss.str());
     }
 
-    // Condições para "multi"
-    std::vector<std::tuple<double, double, double, std::string>> multi_conditions = {
-        {1.0, 1, 70, "Ar"},
-        {1.0, 1, 70, "Ar"},
-        {3.0, 2, 70, "Ar"},
-        {1.0, 1, 90, "Kr"},
-        {1.0, 1, 90, "Kr"}
-    };
+    std::vector<std::string> multi_names = { "Ar_70_30_1atm_1cm", "Ar_70_30_1atm_1cm", "Ar_70_30_2atm_3cm", "Kr_90_10_1atm_1cm", "Kr_90_10_1atm_1cm" };
+    
+    std::vector<std::tuple<int,int>> limits = {
+        {2500, 3500}, {2200, 3700}, {1500, 2000}, {500, 1000}, {500, 1000}
+    }; 
 
-    std::vector<std::string> multinames;
-    for (const auto& [window, pressure, proportion, type] : multi_conditions) {
-        std::ostringstream oss2;
-        oss2 << type << "_" << proportion << "_" << (100 - proportion)
-             << "_" << pressure << "atm_" << window << "cm";
-        multinames.push_back(oss2.str());
-    }
+    
 
-    // Abrir arquivos
     TFile* file = TFile::Open("no_gain.root");
-    TTree* tree = (TTree*) file->Get("no_gain");
-
     TFile* file2 = TFile::Open("ganho.root");
-    TTree* gain_tree = (TTree*) file2->Get("multiplication");
 
-    // Verifica se tamanhos batem
-    if (names.size() != multinames.size()) {
-        std::cerr << "Erro: tamanhos de 'names' e 'multinames' diferentes!\n";
-        return;
-    }
+    for (size_t nome_idx = 0; nome_idx < names.size(); ++nome_idx) {
+        std::cout << "Processando: " << names[nome_idx] << std::endl;
+        
+        int xmin = std::get<0>(limits[nome_idx]);
+        int xmax = std::get<1>(limits[nome_idx]);
+        TF1* gauss = new TF1("gauss", "gaus", xmin, xmax);
 
-    // Loop com índice para percorrer ambos simultaneamente
-    for (size_t i = 0; i < names.size(); ++i) {
-        const std::string& nome = names[i];
-        const std::string& mult_nome = multinames[i];
 
-        std::cout << "Processando: " << nome << " e " << mult_nome << std::endl;
+        // Abrir a TTree de elétrons primários
+        TTree* tree = (TTree*) file->Get(names[nome_idx].c_str());
+        if (!tree) {
+            std::cerr << "Erro: não encontrou TTree '" << names[nome_idx] << "' em no_gain.root\n";
+            continue;
+        }
+
+        // Abrir a TTree de ganhos
+        TTree* gain_tree = (TTree*) file2->Get(multi_names[nome_idx].c_str());
+        if (!gain_tree) {
+            std::cerr << "Erro: não encontrou TTree '" << multi_names[nome_idx] << "' em ganho.root\n";
+            continue;
+        }
 
         int nelectrons = 0;
-        tree->SetBranchAddress(nome.c_str(), &nelectrons);
-
         int ganho = 0;
-        gain_tree->SetBranchAddress(mult_nome.c_str(), &ganho);
 
-        TH1F* pdf = new TH1F(("pdf_" + nome).c_str(), "Gain PDF", 1000, 0, 600);
+        tree->SetBranchAddress("gain", &nelectrons);
+        gain_tree->SetBranchAddress("gain", &ganho);
+
+        TH1F* pdf = new TH1F(("pdf_" + multi_names[nome_idx]).c_str(), "Gain PDF", 300, 0, 200);
         Long64_t nentries_gain = gain_tree->GetEntries();
         for (Long64_t j = 0; j < nentries_gain; ++j) {
             gain_tree->GetEntry(j);
@@ -78,7 +75,8 @@ void input_gain() {
         }
         pdf->Scale(1.0 / pdf->Integral(), "width");
 
-        TH1F* spectra = new TH1F(("spectra_" + nome).c_str(), nome.c_str(), 512, 0, 2 * 4096);
+        TH1F* spectra = new TH1F(("spectra_" + names[nome_idx]).c_str(), names[nome_idx].c_str(), 512, 0,4096);
+        TH1F* primary = new TH1F(("primary_" + names[nome_idx]).c_str(), "Primary Electrons", 512, 0, 1024);
 
         Long64_t nentries = tree->GetEntries();
         for (Long64_t j = 0; j < nentries; ++j) {
@@ -86,19 +84,40 @@ void input_gain() {
 
             int sum = 0;
             for (int k = 0; k < nelectrons; ++k) {
-                sum += pdf->GetRandom();  // simula o ganho de cada elétron
+                sum += pdf->GetRandom();
             }
             spectra->Fill(sum);
         }
 
-        TCanvas* c = new TCanvas(("c_" + nome).c_str(), "Canvas", 800, 600);
+        TCanvas* c_pdf = new TCanvas(("c_pdf_" + multi_names[nome_idx]).c_str(), "PDF Canvas", 800, 600);
+        pdf->GetYaxis()->SetTitle("Probability");
+        pdf->GetXaxis()->SetTitle("Multiplication Factor");
+        pdf->Draw();
+        c_pdf->SetLogy();
+        c_pdf->SaveAs(("../results/pdfs/ganho_" + multi_names[nome_idx] + ".pdf").c_str());
+        c_pdf->SaveAs(("../results/pngs/ganho_" + multi_names[nome_idx] + ".png").c_str());
+        delete c_pdf;
+
+        TCanvas* c = new TCanvas(("c_" + names[nome_idx]).c_str(), "Canvas", 800, 600);
         spectra->GetYaxis()->SetTitle("Counts");
         spectra->GetXaxis()->SetTitle("Spectrum [# electrons]");
-        //spectra->GetXaxis()->CenterTitle();
-        //spectra->GetYaxis()->CenterTitle();
+        spectra->Fit(gauss, "R");
         spectra->Draw();
+        gauss->SetLineColor(kRed);
+        gauss->SetLineWidth(2);
+        gauss->Draw("same");
+        double amplitude = gauss->GetParameter(0);  // altura do pico
+        double mean     = gauss->GetParameter(1);  // média
+        double sigma    = gauss->GetParameter(2);  // sigma
+        // Escreva o valor de sigma perto do pico
+        TLatex latex;
+        latex.SetNDC(false);         // Coordenadas no espaço do eixo (x, y)
+        latex.SetTextSize(0.03);
+        latex.DrawLatex(mean + sigma, amplitude *1.2, Form("FWHM/<E> = %.1f% %", (2.355*sigma/mean)*100));
+   
 
-        c->SaveAs(("../results/"+ nome + ".png").c_str());
+        c->SaveAs(("../results/pdfs/" + names[nome_idx] + ".pdf").c_str());
+        c->SaveAs(("../results/pngs/" + names[nome_idx] + ".png").c_str());
 
         delete pdf;
         delete spectra;
--- photon.cpp
+++ photon.cpp
@@ -19,143 +19,129 @@
 using namespace Garfield;
 using namespace std;
 
-double fundo(TRandom3& rnd, double emax){
-    while(true){
-        double E = rnd.Uniform(1000.,emax);
+double fundo(TRandom3& rnd, double emax) {
+    while (true) {
+        double E = rnd.Uniform(1000., emax);
         double I = (emax = E) / E;
-        double prob = rnd.Uniform(0., (emax - 1000.)/1000.);
-
-        if (prob < I){
-            return E;
-        }
+        double prob = rnd.Uniform(0., (emax - 1000.) / 1000.);
+        if (prob < I) return E;
     }
-
 }
 
-double gamma_ag(TRandom3& rnd){
-    double r = rnd.Uniform(0,100);
-    if(r < 66.7) return 22163.0;
+double gamma_ag(TRandom3& rnd) {
+    double r = rnd.Uniform(0, 100);
+    if (r < 66.7) return 22163.0;
     else if (r < 91.7) return 21983.0;
     else return 25136.0;
 }
 
-double gamma_cu(TRandom3& rnd){
-    double r = rnd.Uniform(0,100);
-    if(r < 66.7) return 8047.8;
+double gamma_cu(TRandom3& rnd) {
+    double r = rnd.Uniform(0, 100);
+    if (r < 66.7) return 8047.8;
     else if (r < 91.7) return 8027.8;
     else return 8905.3;
 }
 
-double gamma_Energy(TRandom3& rnd, double emax){
-    double r = rnd.Uniform(0,100);
-    if(r<10){
-        return fundo(rnd, emax);
-    }
-    else{
-        return gamma_ag(rnd);
-    }
+double gamma_Energy(TRandom3& rnd, double emax) {
+    double r = rnd.Uniform(0, 100);
+    if (r < 10) return fundo(rnd, emax);
+    else return gamma_ag(rnd);
 }
 
-int main(int argc, char * argv[]) {
-
-    TRandom3 rnd(2222); //semente de controle
+int main(int argc, char* argv[]) {
+    TRandom3 rnd(2222); // semente fixa
 
     double emax = 50e3;
 
     TFile* file = TFile::Open("no_gain.root", "RECREATE");
-    TTree* tree = new TTree("no_gain","");
 
+    std::vector<std::tuple<double, double, double, std::string>> conditions = {
+        {1.0, 1, 0.70, "Ar"},
+        {3.0, 1, 0.70, "Ar"},
+        {3.0, 2, 0.70, "Ar"},
+        {1.0, 1, 0.90, "Kr"},
+        {3.0, 1, 0.90, "Kr"},
+    };
 
-    std::vector<std::tuple<double,double,double, std::string>> conditions;
-    conditions.emplace_back(1.0,1,0.70,"Ar");
-    conditions.emplace_back(3.0,1,0.70,"Ar");
-    conditions.emplace_back(3.0,2,0.70,"Ar");
-    conditions.emplace_back(1.0,1,0.90,"Kr");
-    conditions.emplace_back(3.0,1,0.90,"Kr");
+    for (const auto& condition : conditions) {
+        auto [window, pressure, proportion, tipo] = condition;
 
-    for(const auto& condition : conditions){
+        // Nome da TTree
+        std::ostringstream oss;
+        oss << tipo << "_" << int(proportion * 100) << "_" << 100 - int(proportion * 100)
+            << "_" << pressure << "atm_" << window << "cm";
+        std::string treeName = oss.str();
 
-    auto [window, pressure, proportion, tipo] = condition;
+        // Criação da árvore e variável
+        TTree* tree = new TTree(treeName.c_str(), "Número de elétrons primários");
+        int gain = 0;
+        tree->Branch("gain", &gain);
 
+        MediumMagboltz* gas = new MediumMagboltz();
+        gas->SetTemperature(293.15);
+        gas->SetPressure(pressure * 760.);
+        gas->EnableDrift();
+        gas->SetComposition(tipo, proportion, "co2", 1 - proportion);
+        gas->Initialise();
 
-    MediumMagboltz* gas = new MediumMagboltz();
-    gas->SetTemperature(293.15);
-    gas->SetPressure(pressure*760.);
-    gas->EnableDrift();
-    gas->SetComposition(tipo, proportion, "co2", 1 - proportion);
-    gas->Initialise();
-    //gas->EnablePenningTransfer();
-    gas->EnableDebugging();
-    gas->DisableDebugging();
+        constexpr double diameter = 10;
+        double length = window / 2;
+        SolidTube tube(0, 0, 0, 0.5 * diameter, length);
 
-    constexpr double diameter = 10;
-    // Half-length of the cylinder [cm].
-    double length = window/2 ;
-    SolidTube tube(0, 0, 0, 0.5 * diameter, length);
+        GeometrySimple* geo = new GeometrySimple();
+        geo->AddSolid(&tube, gas);
 
-    // Combine gas and box to a simple geometry.
-    GeometrySimple* geo = new GeometrySimple();
-    geo->AddSolid(&tube, gas);
+        ComponentConstant* field = new ComponentConstant();
+        field->SetGeometry(geo);
+        field->SetElectricField(0., 0., 500.);
 
+        Sensor* sensor = new Sensor();
+        sensor->AddComponent(field);
 
-    // Make a component with constant electric field.
-    ComponentConstant* field = new ComponentConstant();
-    field->SetGeometry(geo);
-    field->SetElectricField(0., 0., 500.); 
-    
-    Sensor* sensor = new Sensor();
-	sensor->AddComponent(field);
+        const int nevents = 100000;
+        TrackHeed* track = new TrackHeed();
+        track->SetSensor(sensor);
 
+        for (int idx = 0; idx < nevents; idx++) {
+            gain = 0;  // zera para o novo evento
 
-    double xmin,xmax,ymin,ymax,zmin,zmax;
-    field->GetBoundingBox(xmin,xmax,ymin,ymax,zmin,zmax);
+            double gamma_origin = rnd.Uniform(0, 1);
+            double x, y, z, dx, dy, dz;
 
-    const int nevents = 100000;
+            if (gamma_origin < 0.88) {
+                x = rnd.Uniform(-0.035, 0.035);
+                y = rnd.Uniform(-0.035, 0.035);
+                z = length / 2 - 1e-4;
+                dx = rnd.Uniform(-0.01, 0.01);
+                dy = rnd.Uniform(-0.01, 0.01);
+                dz = -1;
+                track->TransportPhoton(x, y, z, idx, gamma_Energy(rnd, emax), dx, dy, dz, gain);
+            } else {
+                x = rnd.Uniform(-0.035, 0.035);
+                y = rnd.Uniform(-0.035, 0.035);
+                z = -length + 1e-4;
+                dx = rnd.Uniform(-1, 1);
+                dy = rnd.Uniform(-1, 1);
+                dz = rnd.Uniform(0, 1);
+                track->TransportPhoton(x, y, z, idx, gamma_cu(rnd), dx, dy, dz, gain);
+            }
 
-    int nef = 0;
-    std::ostringstream oss;
-    oss << tipo << "_" << proportion << "_" << 100 - proportion << "_" << pressure << "atm_" << window << "cm";
-    std::string resultado = oss.str();
-    tree->Branch(resultado.c_str(),&nef);
+            if (idx % 10000 == 0) cout << idx << "/" << nevents << " (" << treeName << ")" << endl;
 
-    TrackHeed* track = new TrackHeed();
-    track->SetSensor(sensor);
+            if (gain > 0) tree->Fill();
+        }
 
-    for(int idx =0; idx < nevents; idx++){
+        tree->Write();
+        cout << "TTree " << treeName << " escrita com " << tree->GetEntries() << " entradas." << endl;
 
-        double gamma_origin = rnd.Uniform(0,1);
-
-        if (gamma_origin < 0.88){ //tem mais fótons do tubo de raios-X que de cobre
-        double x = 0. + rnd.Uniform(-0.035,0.035);
-        double y = 0. + rnd.Uniform(-0.035,0.035);
-        double z = length/2 - 1e-4; //garantir que o fóton está sendo emitido dentro do volume de gás mas logo acima da região de deriva
-        double dx = rnd.Uniform(-0.01,0.01);//fóton chegnado quase
-        double dy = rnd.Uniform(-0.01,0.01);//paralelo ao gás
-        double dz = -1; 
-        track->TransportPhoton(x,y,z,idx,gamma_Energy(rnd, emax), dx,dy,dz,nef);
+        delete gas;
+        delete geo;
+        delete field;
+        delete sensor;
+        delete track;
     }
-        else{
-        double x = 0. + rnd.Uniform(-0.035,0.035);
-        double y = 0. + rnd.Uniform(-0.035,0.035);
-        double z = -length + 1e-4; //garantir que o fóton está sendo emitido logo acima da camada de cobre em meio angulo sólido
-        double dx = rnd.Uniform(-1,1);//fóton saindo com 
-        double dy = rnd.Uniform(-1,1);//meio angulo sólido
-        double dz = rnd.Uniform(0,1); 
-        track->TransportPhoton(x,y,z,idx,gamma_cu(rnd), dx,dy,dz,nef);
-        }
-        if (idx % 10000 == 0){
-            cout << idx << "/100000" << endl;
-        }
-        if(nef > 0){
-            tree->Fill();
-        }
-}
-    
 
-}
-    tree->Write();
     file->Close();
-
-    cout << "Terminou !" << endl;
+    cout << "Todas as árvores foram salvas em no_gain.root" << endl;
     return 0;
 }
