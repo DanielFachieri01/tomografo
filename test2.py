e4d0c6e73a0416debe170abdad149871d50a1427
Daniel Fachieri <138767767+DanielFachieri01@users.noreply.github.com>
2025-09-23 17:56:29 -0300

    precisa ajustar coisa

--- test2.py
+++ test2.py
@@ -12,9 +12,18 @@ cor10 = "#FFFFFF"
 
 root = Tk()
 Style = ttk.Style()
-
+posx0 = 0.07     # coluna 1 (labels)
+posx1 = 0.14     # coluna 2 (entries)
+posx_btn = 0.28  # coluna 3 (botões)
+posy = 0.3 
+labelw = 0.05
+entryw = 0.08
+btnw = 0.12      # largura uniforme dos botões
+h = 0.04
+offset_y = 0.06  # espaço vertical entre linhas
+ramp_time = 20#
 class Application:
-    style = ttk.Style()
+
     def __init__(self):
         self.root = root
         self.style = Style
@@ -40,17 +49,16 @@ class Application:
             # Envia comandos iniciais
             self.multimeter.write(":SENSe:FUNCtion 'CURRent'")
             self.multimeter.write(":SENSe:CURRent:DC:RANGe:AUTO OFF")
+            self.multimeter.write(":SENSe:CURRent:DC:RANGe 1e-7") 
             self.multimeter.write(":SYSTem:ZCHeck OFF")
+            #self.multimeter.write(":SOURce:VOltage:LEVel:Ramp:TIME 20")
             # Atualiza status na GUI
             self.status_label.config(text="Conectado Keithley 6517B", fg="green")
-            # Exemplo de log local em arquivo
-            with open("log.txt", "a") as log:
-                log.write(":SENSe:FUNCtion 'CURRent'\n")
-                log.write(":SENSe:CURRent:DC:RANGe:AUTO OFF\n")
-                log.write(":SYSTem:ZCHeck OFF\n")
+
         except Exception as e:
                 self.status_label.config(text=f"Erro: {e}", fg="red")
         
+
     def on_exit(self):
         # Fecha conexão se ainda estiver aberta
         if self.multimeter is not None:
@@ -67,15 +75,7 @@ class Application:
                        background=[("active", "#005F99")])
         self.style.theme_use('default')
 
-        posx0 = 0.07     # coluna 1 (labels)
-        posx1 = 0.14     # coluna 2 (entries)
-        posx_btn = 0.28  # coluna 3 (botões)
-        posy = 0.3 
-        labelw = 0.05
-        entryw = 0.08
-        btnw = 0.12      # largura uniforme dos botões
-        h = 0.04
-        offset_y = 0.06  # espaço vertical entre linhas
+
 
         # --- Labels e Entradas ---
         self.lb_v0 = Label(self.root, text='V0', bg=cor10)
@@ -88,7 +88,7 @@ class Application:
         self.v1 = Entry(self.root)
         self.v1.place(relx=posx1, rely=posy + offset_y, relwidth=entryw, relheight=h)
 
-        self.lb_passo = Label(self.root, text='Passo', bg=cor10)
+        self.lb_passo = Label(self.root, text='ΔV', bg=cor10)
         self.lb_passo.place(relx=posx0, rely=posy + 2*offset_y, relwidth=labelw, relheight=h)
         self.passo = Entry(self.root)
         self.passo.place(relx=posx1, rely=posy + 2*offset_y, relwidth=entryw, relheight=h)
@@ -100,26 +100,29 @@ class Application:
         #Stop
         self.stop = Button(self.root, text="Stop", relief="groove", command=self.parar_varredura)
         self.stop.place(relx=posx_btn, rely=posy + offset_y, relwidth=btnw, relheight=h)
-        #Save
-        self.save_btn = Button(self.root, text="Salvar como", relief="groove", command=self.salvar_como)
+        #Save data
+        self.save_btn = Button(self.root, text="Save Data", relief="groove", command=self.salvar_como)
         self.save_btn.place(relx=posx_btn, rely=posy + 2*offset_y, relwidth=btnw, relheight=h)
+        #Save Graph        
+        self.graph_btn = Button(self.root, text="Save Graph", relief="groove", command=self.save_graph)
+        self.graph_btn.place(relx=posx_btn, rely=posy + 3*offset_y, relwidth=btnw, relheight=h)
         #Exit
         self.exit_btn = Button(self.root, text="Exit", relief="groove", command=self.root.destroy)
-        self.exit_btn.place(relx=posx_btn, rely=posy + 3*offset_y, relwidth=btnw, relheight=h)
+        self.exit_btn.place(relx=posx_btn, rely=posy + 5*offset_y, relwidth=btnw, relheight=h)
         # Botão pra conectar
-        self.bt_connect = Button(self.root, text="Conectar", command=self.conectar, relief="groove")
+        self.bt_connect = Button(self.root, text="Connect", command=self.conectar, relief="groove")
         self.bt_connect.place(relx=0.15, rely=0.1, relwidth=0.2, relheight=0.07)
         # Label de status
         self.status_label = Label(self.root, text="Status: Desconectado", fg="red", font=("Arial", 12, "bold"))
         self.status_label.place(relx=0.1, rely=0.2, relwidth=0.3, relheight=0.07)
         # Botão Limpar
-        self.bt_clear = Button(self.root, text="Limpar", command=self.limpar, relief="groove")
+        self.bt_clear = Button(self.root, text="Clear", command=self.limpar, relief="groove")
         self.bt_clear.place(relx=posx_btn, rely=posy + 4*offset_y, relwidth=btnw, relheight=h)
 
     def fundo(self):
         # Carregar a imagem
 
-        self.bg_image = Image.open(r"C:\Users\megat\OneDrive\Área de Trabalho\IFUSP\imagens\logo_simples.jpg")
+        self.bg_image = Image.open(r"C:\Users\Hepic\Desktop\6517B\Daniel\logo_simples.jpg")
         ratio = self.bg_image.height/self.bg_image.width   
         self.bg_image = self.bg_image.resize((300, int(ratio*300)))  # redimensiona pro tamanho da janela
         self.bg_photo = ImageTk.PhotoImage(self.bg_image)
@@ -138,17 +141,22 @@ class Application:
         self.canvas.get_tk_widget().place(relx=0.4, rely=0.0, relwidth=0.6, relheight=1.0)
 
     def iniciar_varredura(self):
+        ##começar do zero
+        self.multimeter.write(":SYSTem:ZCHeck OFF")
+        self.iniciar_rampdown()
+
+
         # Ler valores das entradas
         self.v0_val = float(self.v0.get())
         self.v1_val = float(self.v1.get())
-        self.passo_val = int(self.passo.get())
+        self.dv = int(self.passo.get())
 
-        self.x_values = np.linspace(self.v0_val, self.v1_val, self.passo_val)
+        self.x_values = np.arange(self.v0_val, self.v1_val+1, self.dv)
+
         # Configurações iniciais
-        self.multimeter.write("*RST")
-        self.multimeter.write(":SOUR:VOLT:ILIM 1e-4")   # limite de corrente (segurança)
-        self.multimeter.write(":SOUR:VOLT:STAT ON")     # liga a alta tensão
-        self.multimeter.write(":SENS:FUNC 'CURR'")      # mede corrente
+        self.multimeter.write(":OUTput1:STATe ON")
+        self.multimeter.write(":SOURce:VOLTage:MCONnect ON ")     # liga a alta tensão
+        self.multimeter.write(":SOURce:VOLTage:RANGe MAXimum")    #ajusta o range da alta tensão
 
         self.y_values = []
 
@@ -157,31 +165,44 @@ class Application:
         self.dynamic_x = []
         self.dynamic_y = []
         self.running = True
+
+        self.inicio()
         self.plot_varredura()
 
     def plot_varredura(self):
-        if not self.running or self.current_index >= len(self.x_values):
-            return
+        if self.running and self.current_index < len(self.x_values):
+            # Adiciona próximo ponto
+            self.multimeter.write(f":SOURce:VOltage:LEVel:IMMediate:AMPLitude {self.x_values[self.current_index]}")
+            self.dynamic_x.append(-1*float(self.multimeter.query(":SOUR:VOLT?"))) #salvando o valor de tensão que o keithley tá mandando
+            self.dynamic_y.append(float(self.multimeter.query(":MEAS:CURR?").split(',')[0][:-4])*1e9) #salvando o valor de corrente que o keithley tá medindo
+            self.current_index += 1
 
-        # Adiciona próximo ponto
-        self.multimeter.write(f":SOUR:VOLT {self.x_values[self.current_index]}")
-        self.dynamic_x.append(float(self.multimeter.query(":SOUR:VOLT?"))) #salvando o valor de tensão que o keithley tá mandando
-        self.dynamic_y.append(float(self.multimeter.query(":MEAS:CURR?"))) #salvando o valor de corrente que o keithley tá medindo
-        self.current_index += 1
+            # Atualiza gráfico
+            self.ax.clear()
+            self.ax.plot(self.dynamic_x , self.dynamic_y, color='Black', marker='o', markerfacecolor = "Red")
+            self.ax.set_xlabel("ΔV GEM (V)")
+            self.ax.set_ylabel("Corrente (nA)")
+            self.ax.set_title("Caracterização")
+            self.canvas.draw()
 
-        # Atualiza gráfico
-        self.ax.clear()
-        self.ax.plot(self.dynamic_x, self.dynamic_y, color='Black', marker='o', markerfacecolor = "Red")
-        self.ax.set_xlabel("Tensão (V)")
-        self.ax.set_ylabel("Corrente (nA)")
-        self.ax.set_title("Caracterização")
-        self.canvas.draw()
-
-        # Chama novamente após 100ms
-        self.root.after(100, self.plot_varredura)
+            # Chama novamente após 100ms
+            self.root.after(50, self.plot_varredura)
+        else:
+            self.runnig = False
+            self.multimeter.write(":SYSTem:ZCHeck ON")
+            self.multimeter.write(":OUTput1:STATe OFF")
+            self.multimeter.write()
 
     def parar_varredura(self):
         self.running = False
+
+    def save_graph(self):
+        filename = filedialog.asksaveasfilename(
+            defaultextension=".png",
+            filetypes=[("PNG files", "*.png"), ("Todos os arquivos", "*.*")],
+            title="Salvar figura")
+        if filename:
+            self.fig.savefig(filename)
 
     def salvar_como(self):
         if not hasattr(self, "dynamic_x") or len(self.dynamic_x) == 0:
@@ -198,9 +219,32 @@ class Application:
             if filename.endswith(".csv"):
                 np.savetxt(filename, data, delimiter=",", header="Tensão (V), Corrente (nA)", comments="")
 
+    def iniciar_rampdown(self):
+        
+        tensao_inicial = float(self.multimeter.query(":SOUR:VOLT?").strip())
+        self.rampdown_vals = list(np.linspace(tensao_inicial, 0, 100))  # até menor que 0
+        self.rampdown_step()  
 
-
+    def rampdown_step(self):
+        if self.rampdown_vals:
+            volt_value = self.rampdown_vals.pop(0)
+            self.multimeter.write(f":SOUR:VOLT {volt_value}")
+            self.root.after(100, self.rampdown_step)
+        else:
+            return
+    
+    def inicio(self):
+        self.ramp_vals = list(np.linspace(0,self.v0_val,100))
+        self.ramp_step()
+    
+    def ramp_step(self):
+        if self.ramp_vals:
+            self.multimeter.write(f":SOUR:VOLT {self.ramp_vals.pop(0)}")
+            self.root.after(5,self.ramp_step)
+        else:
+            return 
     def limpar(self):
+        self.iniciar_rampdown()
         # Limpa os dados
         self.x_values = []
         self.y_values = []
@@ -210,8 +254,7 @@ class Application:
         # Reseta o gráfico
         self.ax.clear()
         self.canvas.draw()
-        self.iniciar_grafico()
-
+        #self.iniciar_grafico()
 
 
 
